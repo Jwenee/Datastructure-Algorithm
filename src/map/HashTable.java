1e0259a3125df3c21d0246312a6b2dfe7da63c8f
jwen <283602670@qq.com>
2021-08-08 09:41:35 +0800

    commit

--- src/map/HashTable.java
+++ src/map/HashTable.java
@@ -4,19 +4,21 @@ import java.util.TreeMap;
 
 public class HashTable<K, V> {
 
+    private final int[] capacity = {
+            53,97,193,389,769,1543,3079,6151,12289,24593,49157,98317,196613,393241,786433,1572869,
+            3145739,6291469,12582917,25165843,50331653,100663319,201326611,402653189,805306457,1610612741
+    };
+
     private static final int upperTol = 10;
     private static final int lowerTol = 2;
-    private static final int initCapacity = 7;
+    private int capacityIndex = 0;
 
     private TreeMap<K, V>[] hashTable;
     private int M;
     private int size;
 
     public HashTable() {
-        this(initCapacity);
-    }
-    public HashTable(int M) {
-        this.M = M;
+        this.M = capacity[capacityIndex];
         size = 0;
         hashTable = new TreeMap[M];
         for (int i = 0; i < M; i++) {
@@ -40,8 +42,9 @@ public class HashTable<K, V> {
             map.put(key, value);
             size++;
 
-            if (size >= upperTol * M)
-                resize(2 * M);
+            if (size >= upperTol * M && capacityIndex + 1 < capacity.length)
+                capacityIndex++;
+                resize(capacity[capacityIndex]);
         }
     }
 
@@ -51,8 +54,9 @@ public class HashTable<K, V> {
         if (map.containsKey(key)) {
             ret = map.remove(key);
             size--;
-            if (size < lowerTol * M && M / 2 >= initCapacity)
-                resize(M / 2);
+            if (size < lowerTol * M && capacityIndex - 1 >= 0)
+                capacityIndex--;
+                resize(capacity[capacityIndex]);
         }
         return ret;
     }
